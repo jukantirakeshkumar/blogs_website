8f22b095f8ccdd9093cdd1581bb003845190c610
Jukanti Rakesh kumar <82836961+jukantirakeshkumar@users.noreply.github.com>
2023-10-07 12:35:58 +0530

    Add files via upload

---
+++ User-Form-main/login.js
@@ -0,0 +1,51 @@
+let inputLabels = Array.from(document.querySelectorAll(".inputLabel"));
+let inputs = Array.from(document.querySelectorAll('.textInput'));
+
+// On Page Load Focuses on 1st Input Element
+
+window.addEventListener('load', function() {
+    inputs[0].focus();
+})
+
+// Login for Label Animation for Inputs
+
+inputs.forEach((input) => {
+
+    input.addEventListener('focus', function() {
+        inputLabels.forEach((label) => {
+            if (this.dataset.token === label.dataset.token) {
+                label.classList.add('active');
+            }
+        });
+    });
+
+    input.addEventListener("blur", function () {
+        inputLabels.forEach((label) => {
+            if (this.dataset.token === label.dataset.token) {
+                if (!this.value) {
+                    label.classList.remove("active");
+                }
+            }
+        });
+    });
+
+})
+
+
+// Form Submitting 
+
+function loginFormSubmit(e) {
+    e.preventDefault();
+    
+    // Checking if all inputs are given properly. Just logging them.
+
+    inputs.forEach((input) => {
+        if (input.value) {
+            console.log(`${input.name} : ${input.value}`);
+        }
+    })
+
+    window.location.href = "C:/Users/Asus/OneDrive/Documents/rakesh/project/User-Form-main/User-Form-main/index.html";
+
+    // Above is just a sample, just edit and do waht you like.
+}
